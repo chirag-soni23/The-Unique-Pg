91ec171aab7448b332eac5fe0ae1c63747feda1b
Chirag Soni <csoni0693@gmail.com>
2025-10-11 10:00:38 +0530

    add get method in backend folder

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -4,5 +4,6 @@ import { handleContactForm } from "../controllers/contactController.js";
 const router = express.Router();
 
 router.post("/contact", handleContactForm);
+router.get("/",<div>Backend is live.</div>)
 
 export default router;
