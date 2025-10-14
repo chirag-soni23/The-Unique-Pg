bea2b4f20cc40957a41d0444b25919f3e74eb8fc
Chirag Soni <csoni0693@gmail.com>
2025-10-14 09:57:49 +0530

    modifies

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -4,6 +4,7 @@ import { handleContactForm } from "../controllers/contactController.js";
 const router = express.Router();
 
 router.post("/contact", handleContactForm);
-router.get("/",<div>Backend is live.</div>)
-
+router.get("/", (req, res) => {
+  res.send("Backend is Live!");
+});
 export default router;
