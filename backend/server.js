00f221b45b33427a4fea102b88c716332f79e34e
Chirag Soni <csoni0693@gmail.com>
2025-10-21 09:26:54 +0530

    fixed cors issues

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: ["https://the-unique-pg.vercel.app", "http://localhost:8080"],
-    credentials: true,
+    methods: ["GET", "POST"],
   })
 );
 
